7ed850515fbf4987532d1b81fef2c682eb483077
Julian <julianlx@hotmail.com>
2020-12-15 14:54:27 -0500

    changing if

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,10 @@
 
 
 def no_c(my_string):
-    x = len(my_string)
-    for i in range(0, x):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            copy = my_string[0:i] + my_string[i + 1:x]
+
+    copy = ""
+    for i in my_string:
+        if i not in 'Cc':
+            copy = copy + i
 
     return(copy)

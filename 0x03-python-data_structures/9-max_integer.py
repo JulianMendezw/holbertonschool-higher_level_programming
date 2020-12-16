424df6eb756b658e7eb559c0893e71a652a0d212
Julian <julianlx@hotmail.com>
2020-12-15 23:03:38 -0500

    changin asing to j

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,8 @@
 
 def max_integer(my_list=[]):
     if my_list:
-        j = 0
+        j = my_list[0]
         for i in my_list:
             if i > j:
                 j = i
-        return(j)
-    return(None)
+    return(j if my_list else None)

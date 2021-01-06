d8e17d99bfa466a48c42b9a31962a51e96905d0f
Julian <julianlx@hotmail.com>
2021-01-05 23:21:29 -0500

    append

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,12 +23,10 @@ class Square:
 
     @property
     def size(self):
-        """a class Square that defines a square"""
         return self.__size
 
     @size.setter
     def size(self, size):
-        """a class Square that defines a square"""
         self.__size = size
 
         if (type(size) is not int):
@@ -37,21 +35,7 @@ class Square:
         if (size < 0):
             raise ValueError("size must be >= 0")
 
-    @property
-    def position(self):
-        """a class Square that defines a square"""
-        return self.__position
-
-    @position.setter
-    def position(self, position):
-        """a class Square that defines a square"""
-        self.__position = position
-
-    if (size < 0):
-        raise ValueError("size must be >= 0")
-
     def my_print(self):
-        """a class Square that defines a square"""
 
         if self.__size == 0:
             print("")
@@ -65,3 +49,14 @@ class Square:
                 for j in range(self.__size):
                     print("#", end='')
                 print("")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        self.__position = position
+
+        if (len(position) is not 2):
+            raise TypeError("position must be a tuple of 2 positive integers")

bbe501f1cfed7691b655eb3a928ee12229969ec9
Julian <julianlx@hotmail.com>
2021-01-12 01:16:52 -0500

    append

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,7 +11,6 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        self.print_symbol = Rectangle.print_symbol
         Rectangle.number_of_instances += 1
 
     @property

74a469bf13494dcca5af2ffa479e809d3c051e87
Julian <julianlx@hotmail.com>
2021-01-19 21:20:06 -0500

    new subclass square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Contain a class Square """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ a class Square that inherits from Rectangle (9-rectangle.py) """
+
+    def __init__(self, size):
+        """ Atributes of Square """
+        self.__size = size
+
+        self.integer_validator('Square', size)
+
+    def area(self):
+        """ Area of a square """
+        return self.__size ** 2

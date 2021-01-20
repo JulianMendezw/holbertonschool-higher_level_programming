599462daa58b9435f9b8b9053cef1c09c8ac8195
Julian <julianlx@hotmail.com>
2021-01-19 20:38:29 -0500

    Are method implemented

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Contain a class BaseGeometry """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Define an rectangle
+
+        Args:
+            BaseGeometry (class): Class BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """ Atributtes of the class Rectangule """
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+
+    def area(self):
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return str(self.__width * self.__height)

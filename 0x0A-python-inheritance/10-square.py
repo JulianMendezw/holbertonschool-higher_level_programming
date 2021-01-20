22c2e51c7bd3254e997f2ffcfa09745905c1314d
Julian <julianlx@hotmail.com>
2021-01-19 21:21:32 -0500

    Fixing argv passing to integer validator

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,13 +6,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ a class Square that inherits from Rectangle (9-rectangle.py) """
+    """ A class Square that inherits from Rectangle (9-rectangle.py) """
 
     def __init__(self, size):
         """ Atributes of Square """
         self.__size = size
 
-        self.integer_validator('Square', size)
+        self.integer_validator('square', size)
 
     def area(self):
         """ Area of a square """

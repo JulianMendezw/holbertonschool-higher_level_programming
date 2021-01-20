93c6cc9e29a09d53ff1dbbbfa064b1296818ecb5
Julian <julianlx@hotmail.com>
2021-01-19 23:35:02 -0500

    this a rebel class

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" emty doctstring """
+
+
+class MyInt(int):
+    """ A class MyInt that inherits from int """
+    def __eq__(self, other):
+        return self.real != other
+
+    def __ne__(self, other):
+        return self.real == other

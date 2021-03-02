f6e2faf89075f25dd2652e3630d9d1879ca7ec29
Julian <julianlx@hotmail.com>
2021-03-02 15:19:19 -0500

    script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
--- script that creates the database hbtn_0d_2 and the user user_0d_2.
--- Only if no exist
+  -- Creates the database hbtn_0d_2 and the user user_0d_2
+-- Query to perform operation
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d2.* TO 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
+      IDENTIFIED BY 'user_0d_2_pwd';
 

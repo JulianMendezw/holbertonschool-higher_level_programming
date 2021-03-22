7beafa937f3bced13781610ce951e5d08b6a7851
Julian <julianlx@hotmail.com>
2021-03-22 13:38:30 -0500

    argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,8 @@
 const argv = process.argv;
 if (argv.length < 3) {
   console.log('No argument');
-} else if (argv.length === 3) {
-	console.log('Argument found');
+} else if (argv.length == 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }

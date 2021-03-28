d2b9bff436315ff3644b13efb7cfdab406666f83
Julian Mendez <julianlx@hotmail.com>
2021-03-27 22:11:22 -0500

    class city

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a python file that contains the class definition of a State and an
+instance Base = declarative_base()"""
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+
+class City(Base):
+    """ A class City that inherits from Base """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    email = Column(String(128), nullable=False)

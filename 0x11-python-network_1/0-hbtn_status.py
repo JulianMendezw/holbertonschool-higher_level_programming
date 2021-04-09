13d82c9868f0d72e569a0866a0fe5c0b080071e4
Julian <julianlx@hotmail.com>
2021-04-09 14:13:37 -0500

    fixing comments

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ script that takes in a URL, sends a request to the URL and displays the
-    value of the X-Request-Id variable found in the header of the response.
-    import urllib.request """
+    value of the X-Request-Id variable found in the header of the response. """
+import urllib.request
 
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print('Body response:')
-    print('    - type: {}'.format(type(html)))
-    print('    - content: {}'.format(html))
-    print('    - utf8 content: {}'.format(html.decode('utf8')))
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf8')))

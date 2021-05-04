f88bd81e0bcf0cf8825a813fc49db05ce2eb1035
Julian <julianlx@hotmail.com>
2021-05-04 14:13:50 -0500

    printing a code status response

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,8 @@ const argv = process.argv;
 const request = require('request');
 request(argv[2], function (error, response) {
   if (error) {
-    console.error('code: ', error); // Print the error if one occurred
+    console.error('code: ', error);
   } else {
-    console.log('code: ', response.statusCode); // Print the response status code if a response was received
+    console.log('code: ', response.statusCode);
   }
 });

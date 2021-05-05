e62fe0c6c8e6d6846d13aae8d500b61077b3cebe
Julian <julianlx@hotmail.com>
2021-05-04 21:38:34 -0500

    searching by character id

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,17 +3,19 @@ const argv = process.argv;
 const request = require('request');
 const url = argv[2];
 const characterID = 18;
-const characList = 'https://swapi-api.hbtn.io/api/people/' + characterID + '/';
+
 let count = 0;
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const results = JSON.parse(body).results;
-    for (let i = 0; results[i]; i++) {
-      if (results[i].characters.indexOf(characList) > -1) {
-        count = count + 1;
+    const rbody = JSON.parse(body);
+    for (const i of rbody.results) {
+      for (const j of i.characters) {
+        if (j.search(characterID) > 0) {
+          count++;
+        }
       }
     }
     console.log(count);
